1baa095a470fbd5055aecde745ba90c1d6822be5
Paula Moratalla <paulamrodriguez723@gmail.com>
2022-05-10 15:20:22 +0200

    experience routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use('/user', require('./user.routes'))
 router.use('/auth', require('./auth.routes'))
+router.use('/experience', require('./experience.routes'))
 
 module.exports = router;
