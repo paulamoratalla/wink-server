20692814ba76fd1ac7580e42b29ca6f23736c9fd
Roberto D. Ezquerro <roberto.ezquerro@gmail.com>
2022-05-18 18:18:46 +0200

    De todo

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,46 +1,34 @@
 const router = require('express').Router()
 const User = require('./../models/User.model')
 const { isAuthenticated } = require('../middleware/jwt.middleware')
-
-// Get all users 
-router.get('/all', (req, res) => {
-
+// Get all users
+router.get('/all', isAuthenticated, (req, res) => {
     User
         .find()
         .select()
         .then(users => res.json(users))
         .catch(err => res.status(500).json(err))
 })
-
-
 // Get my profile
 router.get('/profile', isAuthenticated, (req, res) => {
-
     const { _id } = req.payload
-
     User
         .findById(_id)
         .then(user => res.json(user))
         .catch(err => res.status(500).json(err))
 })
-
-// Delete user 
+// Delete user
 router.delete('/:userId/delete', isAuthenticated, (req, res) => {
-
     const { userId } = req.params
-
     User
         .findByIdAndDelete(userId)
         .then(response => res.json(response))
         .catch(err => res.status(500).json(err))
 })
-
 // Edit user
-router.put('/:userId/edit', (req, res) => {
-
+router.put('/:userId/edit', isAuthenticated, (req, res) => {
     const { name, email, birth, identity, profileImg, city, interestedGender, height, exercise, zodiac, education, drink, smoke, lookingFor, children, religion, political } = req.body
     const { userId } = req.params
-
     const features = {
         height: height,
         exercise: exercise,
@@ -53,59 +41,50 @@ router.put('/:userId/edit', (req, res) => {
         religion: religion,
         political: political
     }
-
     User
         .findByIdAndUpdate(userId, { name, email, birth, identity, profileImg, city, interestedGender, features })
         .then(user => res.json(user))
         .catch(err => res.status(500).json(err))
 })
-
-//Add to people
-router.post('/:usersId/add-people', (req, res, next) => {
-
+//Add to matches
+router.post('/:userId/add', isAuthenticated, (req, res, next) => {
     const { userId } = req.params
-    const thisUser = req.payload._id
-
+    const myUser = req.payload._id
     User
-        .findByIdAndUpdate(thisUser, { $addToSet: { people: userId } })
-        .then(user => res.json(user))
+        .findByIdAndUpdate(myUser, { $addToSet: { matches: userId } })
+        .then(() => {
+        })
+        .catch(err => res.status(500).json(err))
+    User
+        .findById(myUser)
+        .then((user1) => {
+            User
+                .findById(userId)
+                .then((user2) => {
+                    if (user1.matches.includes(userId) && (user2.matches.includes(myUser))) {
+                        user1.lovers.push(userId) && user2.lovers.push(myUser)
+                    }
+                })
+                .catch(err => res.status(500).json(err))
+        })
         .catch(err => res.status(500).json(err))
 });
 
-//Add to my matches
-// router.post('/:usersId/add-defmatch', (req, res, next) => {
-
-//     const { userId } = req.params
-//     const thisUser = req.session.currentUser._id
-//     if ()
-
-//         User
-//             .findByIdAndUpdate(thisUser, { $addToSet: { defMatches: userId } })
-//             .then(user => res.json(user))
-//             .catch(err => res.status(500).json(err))
-// });
-
-//Remove from people
-router.post('/:usersId/remove', (req, res, next) => {
-
+router.post('/:usersId/remove', isAuthenticated, (req, res, next) => {
     const { userId } = req.params
     const thisUser = req.payload._id
-
     User
-        .findByIdAndUpdate(thisUser, { $pull: { people: userId } })
+        .findByIdAndUpdate(thisUser, { $pull: { matches: userId } })
         .then(user => res.json(user))
         .catch(err => res.status(500).json(err))
 })
-
 // Get user profile
 router.get('/:userId', isAuthenticated, (req, res) => {
-
     const { userId } = req.params
-
     User
         .findById(userId)
+        .populate('matches lovers')
         .then(user => res.json(user))
         .catch(err => res.status(500).json(err))
 })
 module.exports = router
-
